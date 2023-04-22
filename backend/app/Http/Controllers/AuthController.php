732733d84eb9c71047bab0d2db0ad85a0583f5e1
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2023-04-22 10:42:04 +0200

    Hecho controlador auth. Hecho LoginRequest y RegisterRequest

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
+    public function login(LoginRequest $request){
+
+        $token = $request->user()->createToken('MyApp')->plainTextToken;
+        return response()->json([
+            'success'=>true,
+            'message'=>'Correct login',
+            'data'=>['token'=>$token, 'user'=>$request->user()],],200);
+    }
+
+    public function register(RegisterRequest $request){
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->last_name = $request->input('last_name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        // $token = $user->createToken('MyApp')->plainTextToken(7400); //dos horas
+        $token = $user->createToken('kario')->plainTextToken;
+        return response()->json(['success'=>true,'message'=>'User created', 'data'=>['token'=>$token]],201);
+
+    }
 }
---
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Validator;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.email' => 'El campo correo electrónico debe ser una dirección de correo electrónico válida.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'email.unique' => 'The email has already been taken',
+            'password.min' => 'El campo contraseña debe tener al menos :min caracteres.',
+            'password.confirmed' => 'La confirmación de contraseña no coincide.',
+        ];
+    }
+    
+    protected function failedValidation($validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation failed',
+            'data' => $validator->errors()
+        ], 422));
+    }
+
+}
+    // public function withValidator(Validator $validator)
+    // {
+    //     $validator->after(function ($validator) {
+    //         if (!$this->attemptLogin()) {
+    //             $validator->errors()->add('email', 'Credenciales inválidas');
+    //         }
+    //     });
+    // }
+
+    // public function attemptLogin()
+    // {
+    //     $credentials = $this->only(['email', 'password']);
+    //     return auth()->attempt($credentials);
+    // }
