aabf3313f38623c6b9d331ffe18d74e0d0c4d430
alexis4532 <alexismoyazrt@gmail.com>
2023-04-27 23:29:16 +0200

    modelos y migraciones

--- backend/database/migrations/2023_04_26_231622_create_deck_collaborators_table.php
+++ backend/database/migrations/2023_04_26_231622_create_deck_collaborators_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('deck_collaborators', function (Blueprint $table) {
+            $table->foreignId('fk_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('fk_deck')->constrained('decks')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('deck_collaborators');
     }
 };
--- backend/database/migrations/2023_04_26_231744_create_phrases_table.php
+++ backend/database/migrations/2023_04_26_231744_create_phrases_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('phrases', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('text',255);
+            $table->string('translation',255);
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('phrases');
     }
 };
