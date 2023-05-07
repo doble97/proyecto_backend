71e55c8c53138b8af097648dcaa0489b02179df1
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2023-05-07 22:41:22 +0200

    Preparando para hacer merge con rama de Alexis

--- backend/app/Http/Controllers/DeckController.php
+++ backend/app/Http/Controllers/DeckController.php
@@ -21,7 +21,7 @@ class DeckController extends Controller
         $deck_owner->fk_user = $request->user()->id;
         $deck_owner->fk_deck = $deck->id;
         $deck_owner->save();
-        return response()->json(['successful'=>true, 'message'=>'Creación exitosa', 'data'=>$deck, 'propietario'=>$deck_owner]);
+        return response()->json(['success'=>true, 'message'=>'Creación exitosa', 'data'=>$deck]);
     }
     //DELETE
     function delete(Request $request,string $id){
@@ -29,14 +29,14 @@ class DeckController extends Controller
             try{
                 $deck = Deck::findOrFail($id);
                 $deck->delete();
-                return response()->json(['successful'=>true, 'message'=>'Registro eliminado correctamente'], 204);
+                return response()->json(['success'=>true, 'message'=>'Registro eliminado correctamente'], 204);
 
             }catch (ModelNotFoundException $err){
-                return response()->json(['successful'=>false, 'message'=>'Registro no encontrado'], 404);
+                return response()->json(['success'=>false, 'message'=>'Registro no encontrado'], 404);
 
             }
         }
-        return response()->json(['successful'=>false, 'message'=>'Parametros incorrectos'], 400);
+        return response()->json(['success'=>false, 'message'=>'Parametros incorrectos'], 400);
     }
 
     //READ ALL DECKS
@@ -44,10 +44,10 @@ class DeckController extends Controller
         try{
             $decks= $request->user()->decks;
             // $registros = Deck::whereAs
-            return response()->json(['successful'=>true, 'message'=>'Registros encontrados', 'data'=>$decks]);
+            return response()->json(['success'=>true, 'message'=>'Registros encontrados', 'data'=>$decks]);
 
         }catch (ModelNotFoundException $err){
-            return response()->json(['successful'=>false, 'message'=>'Registro no encontrado'], 404);
+            return response()->json(['success'=>false, 'message'=>'Registro no encontrado'], 404);
 
         }
     }
