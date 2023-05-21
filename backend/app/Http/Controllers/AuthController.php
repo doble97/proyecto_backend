c37b3afb455328014d4a880e5a8ef56ed9714dd3
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2023-05-21 13:21:40 +0200

    Cambios para obtener datos en el dashboard al hacer login

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Deck;
+use App\Models\DeckOwner;
+use App\Models\Friend;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -11,12 +14,30 @@ class AuthController extends Controller
 {
     //
     public function login(loginRequest $request){
-
+        $userId= $request->user()->id;
         $token = $request->user()->createToken('MyApp')->plainTextToken;
+        $pendind_requests = Friend::where('fk_user_receive_request', $userId)->count();
+        $decks = DeckOwner::where('fk_user', $userId)->count();
+        $shared_decks=0;
+        $followed_decks=0;
+        $friends = Friend::where(function($query) use($userId){
+            $query->where('fk_user_receive_request', $userId)
+            ->orWhere('fk_user_send_request', $userId);
+        })
+        // ->where('status','pending')
+        ->count();
         return response()->json([
             'success'=>true,
             'message'=>'Correct login',
-            'data'=>['token'=>$token, 'user'=>$request->user()],],200);
+            'data'=>[
+                'token'=>$token, 
+                'user'=>$request->user(),
+                "friends"=>$friends, 
+                "pending_requests"=>$pendind_requests,
+                "decks"=>$decks,
+                "shared_decks"=>$shared_decks,
+                "followed_decks"=>$followed_decks
+            ],],200);
     }
 
     public function register(registerRequest $request){
